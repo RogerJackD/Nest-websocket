94627acf656d952e4606899b7ed6bb03443c0eb5
RogerJackD <rogerzapata114@gmail.com>
2026-02-12 23:14:55 -0500

    feat(user): add services findOne, create and implement method handleDbErros

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RegisterDto } from 'src/auth/dto/register.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ){}
+
+  async create(registerDto: RegisterDto) {
+
+    try {
+      const newUser = this.userRepository.create({...registerDto});
+      await this.userRepository.save(newUser);
+      return newUser;
+    } catch (error) {
+      this.handleDbErrors(error);
+    }
+  }
+
+  async findOneByEmail(email : string ){
+    const user = await this.userRepository.findOneBy({ email });
+    return user;
   }
 
   findAll() {
@@ -23,4 +45,12 @@ export class UsersService {
   remove(id: number) {
     return `This action removes a #${id} user`;
   }
+
+  private handleDbErrors(error: any) {
+    if(error.code === '23505') throw new BadRequestException(error.detail)
+
+    throw new BadRequestException('ERROR DB EXCEPTIONS CHECK LOGS')
+  }
+
+
 }
