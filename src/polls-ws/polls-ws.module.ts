e8039f0731576167033179a2d0fe6b3e48be500d
RogerJackD <rogerzapata114@gmail.com>
2026-02-12 23:12:21 -0500

    feat: import AuthModule required JwtService config

--- src/polls-ws/polls-ws.module.ts
+++ src/polls-ws/polls-ws.module.ts
@@ -3,10 +3,12 @@ import { PollsWsService } from './polls-ws.service';
 import { PollsWsGateway } from './polls-ws.gateway';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Poll } from './entities/polls.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   providers: [PollsWsGateway, PollsWsService],
   imports: [
+    AuthModule,
     TypeOrmModule.forFeature([Poll])
   ]
 })
