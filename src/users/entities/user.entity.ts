97376b73e9fc9207768dac9fa7c0d225716fbc9c
RogerJackD <rogerzapata114@gmail.com>
2026-02-12 23:15:07 -0500

    feat: add new user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,27 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Role } from "../../common/enums/role.enum";
+
+@Entity({name: 'users'})
+export class User {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({type: "varchar", unique: true})
+    email: string;
+
+    @Column({ type: "varchar"})
+    password: string;
+
+    @Column({type: "varchar", name: "full_name"})
+    fullName: string;
+
+    @Column({type: "enum", enum: Role, default: Role.USER})
+    role: Role;
+
+    @Column({type: 'bool', name: "is_active", default: true})
+    isActive: boolean;
+
+    @CreateDateColumn({name: "create_at"})
+    createAt: Date;
+}
