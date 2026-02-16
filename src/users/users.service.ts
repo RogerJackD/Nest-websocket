238e6814ab0e5e4f640ed3b5dc8952c51e7930da
RogerJackD <rogerzapata114@gmail.com>
2026-02-15 22:38:55 -0500

    feat: implements service to updateProdileImage

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,10 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { RegisterDto } from 'src/auth/dto/register.dto';
+import { AzureStorageService } from 'src/azure-storage/azure-storage.service';
 
 @Injectable()
 export class UsersService {
@@ -12,6 +12,7 @@ export class UsersService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    private readonly azureStorageService: AzureStorageService,
   ){}
 
   async create(registerDto: RegisterDto) {
@@ -45,6 +46,24 @@ export class UsersService {
   remove(id: number) {
     return `This action removes a #${id} user`;
   }
+
+  async updateProfileImage(id: string, file: Express.Multer.File){
+    //searchUser
+    const user =  await this.userRepository.findOneBy({id});
+    if(!user) throw new BadRequestException('User not found');
+
+    if( user.profileImage ){
+      await this.azureStorageService.deleteFile(user.profileImage)
+    }
+
+    //upload new image
+    const imageUrl = await this.azureStorageService.uploadFile(file);
+
+    user.profileImage = imageUrl;
+
+    return this.userRepository.save(user);
+  }
+
 
   private handleDbErrors(error: any) {
     if(error.code === '23505') throw new BadRequestException(error.detail)
