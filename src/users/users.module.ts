7c384a887a1dd9e6341b2a50bc0d8961e4b97c41
RogerJackD <rogerzapata114@gmail.com>
2026-02-15 22:37:20 -0500

    feat: add imports storage service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,12 +3,14 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { AzureStorageModule } from 'src/azure-storage/azure-storage.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
   imports: [
-    TypeOrmModule.forFeature([User])
+    TypeOrmModule.forFeature([User]),
+    AzureStorageModule
   ],
   exports: [UsersService]
 })
