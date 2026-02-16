93aaa88cf0774d0a4980fcd66c379570fa81affe
RogerJackD <rogerzapata114@gmail.com>
2026-02-15 22:38:01 -0500

    feat: add imports & exports

--- src/azure-storage/azure-storage.module.ts
+++ src/azure-storage/azure-storage.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AzureStorageService } from './azure-storage.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  providers: [AzureStorageService]
+  providers: [AzureStorageService],
+  imports: [ConfigModule],
+  exports: [AzureStorageService],
 })
 export class AzureStorageModule {}
