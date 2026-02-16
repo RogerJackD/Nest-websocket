a75a1355f2cc74d63511dd795454d7e7ebd025d2
RogerJackD <rogerzapata114@gmail.com>
2026-02-15 21:40:49 -0500

    feat: add new service azure storage

--- src/azure-storage/azure-storage.module.ts
+++ src/azure-storage/azure-storage.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { AzureStorageService } from './azure-storage.service';
 
-@Module({})
+@Module({
+  providers: [AzureStorageService]
+})
 export class AzureStorageModule {}
